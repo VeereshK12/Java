0a851addc806b7991a17f5182344bff0c75964b0
Veeresh <Veereshbk183@gmail.com>
2016-09-26 22:27:25 +0530

    problem set 1(1&2)

---
+++ MINOR/src/problem2/Checks.java
@@ -0,0 +1,28 @@
+package problem2;
+
+public class Checks {
+
+	boolean checkBaggage(Traveller T){
+		
+		if(T.getfBaggageAmount()>=0&&T.getfBaggageAmount()<=0)
+			return true;
+		else
+			return false;
+		
+	}
+	
+	boolean checkImmigration(Traveller T)
+	{
+		if(T.getiExpiryYear()>=2001&&T.getiExpiryYear()<=2025)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean checkSecurity(Traveller T){
+		if(T.isbNOCStatus()==true)
+			return true;
+		else
+				return false;
+	}
+}
